87a27243965b370b20c68edc14c6a651636c378a
Yunong Liu <yl2494@cornell.edu>
2015-02-01 15:24:24 -0500

    wp_localize_script(): php file url to js

--- user_tag.js
+++ user_tag.js
@@ -2,6 +2,9 @@
  * Created by yunong on 2/1/2015.
  */
 jQuery(document).ready(function($) {
-    var avatarUrl = "http://pngimg.com/upload/fish_PNG1159.png";
+    var number = "001";
+    console.log(userTagData.pluginPath);
+    var avatarUrl = userTagData.pluginPath+"Images/"+ number + ".png";
+    console.log(avatarUrl);
     $('.comment-author:first').append('<img  class="avatar_right" style="-webkit-user-select: none; cursor: zoom-in;" src="'+ avatarUrl +'" width="50" height="50">');
 });
--- user_tag.php
+++ user_tag.php
@@ -10,9 +10,13 @@
 
 
 add_action( 'init', 'user_tag_init' );
+
 function user_tag_init() {
     wp_enqueue_script("jquery");
     wp_enqueue_style( 'fishy', plugins_url( 'user_tag.css', __FILE__ ) );
     wp_enqueue_script( 'userTag', plugins_url( 'user_tag.js', __FILE__ ), array( 'jquery' ) );
+    wp_localize_script('userTag', "userTagData", array(
+        "pluginPath" => plugin_dir_url(__FILE__)
+    ));
 }
 
