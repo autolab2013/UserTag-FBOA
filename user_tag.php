34eb36ce5f9ba595d4731697b6b4219cd760a227
Yunong Liu <yl2494@cornell.edu>
2015-02-01 14:42:58 -0500

    update header

--- user_tag.php
+++ user_tag.php
@@ -1,17 +1,17 @@
 <?php
 /*
  * Plugin Name: User Tag
- * Plugin URI:
+ * Plugin URI: https://github.com/autolab2013/UserTag-FBOA
  * Description: assign different avatars to user based on their information on blogs
  * Author: Riley Liu
  * Version: 0.1
- * Author URI:
+ * Author URI: http://autolab2013.github.io/
  */
 
 add_action('admin_init', 'classification_init');
 
 function classification_init(){
-    wp_enqueue_style( 'user_tag', plugins_url( 'fishy.css', __FILE__ ) );
-    wp_enqueue_script( 'user_tag', plugins_url( 'fishy.js', __FILE__ ), array( 'jquery' ) );
+    wp_enqueue_style( 'user_tag', plugins_url( 'user_tag.css', __FILE__ ) );
+    wp_enqueue_script( 'user_tag', plugins_url( 'user_tag.js', __FILE__ ), array( 'jquery' ) );
 }
 
