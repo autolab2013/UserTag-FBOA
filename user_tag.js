301c33b3616c8857302258727a46157fdbc4b971
FATEX <gzy19910428@gmail.com>
2015-02-01 14:40:47 -0500

    reformat code

--- user_tag.js
+++ user_tag.js
@@ -2,6 +2,6 @@
  * Created by yunong on 2/1/2015.
  */
 jQuery(document).ready(function($) {
-    $('.comment-author:first').append('<img  style="-webkit-user-select: none; cursor: zoom-in;" src="http://pngimg.com/upload/fish_PNG1159.png" width="50" height="50" right=0px>');
-
+    var avatarUrl = "http://pngimg.com/upload/fish_PNG1159.png";
+    $('.comment-author:first').append('<img  class="avatar_right" style="-webkit-user-select: none; cursor: zoom-in;" src="'+ avatarUrl +'" width="50" height="50">');
 });
