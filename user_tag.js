5b0c9013633cad767873645c49f61f1a01b437e8
FATEX <gzy19910428@gmail.com>
2015-02-01 15:24:41 -0500

    add random function to random pics

--- user_tag.js
+++ user_tag.js
@@ -2,6 +2,17 @@
  * Created by yunong on 2/1/2015.
  */
 jQuery(document).ready(function($) {
-    var avatarUrl = "http://pngimg.com/upload/fish_PNG1159.png";
-    $('.comment-author:first').append('<img  class="avatar_right" style="-webkit-user-select: none; cursor: zoom-in;" src="'+ avatarUrl +'" width="50" height="50">');
+    var avatarUrl;
+    var commentAvatars = $('.comment-author');
+    commentAvatars.each(function(){
+        var num = Math.floor(Math.random() * 2) + 1;
+        if(num == 1){
+            avatarUrl = "http://pngimg.com/upload/fish_PNG1159.png";
+        }else {
+            avatarUrl = "http://fc08.deviantart.net/fs70/f/2009/352/a/3/Little_Fish_png_by_AbsurdWordPreferred.png";
+        }
+        $(this).append('<img  class="avatar_right" style="-webkit-user-select: none; cursor: zoom-in;" src="'+ avatarUrl +'" width="50" height="50">');
+    });
+
+    console.log(commentAvatars);
 });
