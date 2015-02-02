32e1f42bafd1c44f53bfcd5266e2d8ebf51e4dd2
FATEX <gzy19910428@gmail.com>
2015-02-01 19:36:56 -0500

    run analyze after a user post a comment

--- user_tag.js
+++ user_tag.js
@@ -4,12 +4,18 @@
 jQuery(document).ready(function($) {
     var number = "001";
     var commentAvatars = $('.comment-author');
-    commentAvatars.each(function(){
+    commentAvatars.each(function() {
         var avatarUrl = userTagData.pluginPath+"Images/";
         var num = Math.floor(Math.random() * 3) + 1;
         avatarUrl += "00" + num + ".png";
         $(this).append('<img  class="avatar_right" style="-webkit-user-select: none; cursor: zoom-in;" src="'+ avatarUrl +'" width="50" height="50">');
-    });
 
+    })
+
+    $('input#submit').click(function() {
+            // Get all the forms elements and their values in one step
+            var values = $('textarea#comment').val();
+    });
     console.log(commentAvatars);
+
 });
--- user_tag.php
+++ user_tag.php
@@ -10,26 +10,35 @@
 
 
 add_action( 'init', 'user_tag_init' );
-add_action( 'init', 'analyze' );
+//add_action( 'init', 'analyze' );
+add_action('comment_post', 'analyze');
 
 function user_tag_init() {
     wp_enqueue_script("jquery");
+    wp_enqueue_script("node");
     wp_enqueue_style( 'userTag', plugins_url( 'user_tag.css', __FILE__ ) );
     wp_enqueue_script( 'userTag', plugins_url( 'user_tag.js', __FILE__ ), array( 'jquery' ) );
     wp_localize_script('userTag', "userTagData", array(
         "pluginPath" => plugin_dir_url(__FILE__)
     ));
-
 }
 
-function analyze() {
+function analyze($commentId) {
+    $commentData = get_comment($commentId);
+    /*echo $commentData->comment_content;
+    //var_dump($commentData);
+    exit();*/
     require_once 'alchemyapi.php';
     $key = file_get_contents(dirname(__FILE__) . '/api_key.txt');
     $alchemyapi = new AlchemyAPI( $key );
     //echo plugin_dir_url(__FILE__);
-    $myText = "I can't wait to integrate AlchemyAPI's awesome PHP SDK into my app!";
+    //$myText = "I can't wait to integrate AlchemyAPI's awesome PHP SDK into my app!";
+    $myText = $commentData->comment_content;
     $response = $alchemyapi->sentiment("text", $myText, null);
     echo "Sentiment: ", $response["docSentiment"]["type"], PHP_EOL;
-    echo "Scroe: ", $response["docSentiment"]["score"], PHP_EOL;
+    if(!empty($response["docSentiment"]["score"])) {
+        echo "Score: ", $response["docSentiment"]["score"], PHP_EOL;
+    }
+    exit();
 }
 
